3f00708973dff1eac9e5a441fec150fbfabbf729
Michael Trachtman <mike@superbmeteor.co>
2022-07-28 09:05:53 -0400

    initial commit

---
+++ src/content.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App'
+import './index.css'
+
+const root = document.createElement('div')
+root.id = 'crx-root'
+document.body.append(root)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   root,
+// )
+
+alert('this runs so long as React is not invoked')
