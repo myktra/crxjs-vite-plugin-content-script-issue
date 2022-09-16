bde8024cd01221a8cc8671ba26c947ce0b82172e
Michael Trachtman <mike@superbmeteor.co>
2022-09-16 17:42:02 -0400

    pre-yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,10 +9,18 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={chrome.runtime.getURL('/vite.svg')} className="logo" alt="Vite logo" />
+          <img
+            src={chrome.runtime.getURL('/vite.svg')}
+            className="logo"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={chrome.runtime.getURL(reactLogo)} className="logo react" alt="React logo" />
+          <img
+            src={chrome.runtime.getURL(reactLogo)}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -27,7 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h3>15</h3>
+      <h3>16</h3>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' assert { type: "json" }
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import manifest from './manifest.json' assert { type: 'json' }
 
 // https://vitejs.dev/config/
 export default defineConfig({
